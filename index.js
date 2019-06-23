f011443f0e243918b26365303cfa66494d0c2c2d
steadfast-network-4426 <djreeves1292@gmail.com>
2019-06-23 02:50:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma(string) {
+  if ('hello'.toUpperCase()) {
+  return "YES INDEED!";
+    }
+
+}
+function logWhisper(string) {
+  console.log('hello');
+}
+
+function logShout(string) {
+  console.log('HELLO');
+}
+
+
